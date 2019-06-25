b26de6728fc59aa2fdd0c48bbce6652c0a007dc5
João Eudes Lima <joaoeudes7@gmail.com>
2019-06-25 14:56:07 -0300

    Update methods api

--- app/controllers/urls.py
+++ app/controllers/urls.py
@@ -1,61 +1,85 @@
+from flask import render_template, request, jsonify
+
 from app import app, db
-from flask import render_template
-from flask import request, jsonify
-import json
-from app.model.tables import User, Dependent 
+from app.model.tables import User
 
 """
     Rota inicial
 """
-@app.route('/index',  methods=['GET'])
-@app.route('/', methods=['GET'] )
+
+
+@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route("/getUser")
+@app.route("/getUser", methods=['GET'])
 def getUser():
-    return 'teste'
+    data = request.get_json()
+
+    user = User.query.filter_by(id=data['id']).first()
+    return jsonify(user.serialize())
+
 
 @app.route('/index/insertUser', methods=['POST'])
-@app.route('/index/insertuser', methods=['POST'])
 def insertUser():
-    
     data = request.get_json()
-    user = User(name=data['name'],email=data['email'], addres=data['address'], fone=data['phone'])
+
+    user = User(name=data['name'], email=data['email'], addres=data['address'], fone=data['phone'])
+
     db.session.add(user)
     db.session.commit()
-    response = {
-        'id' : user.id
-    }
 
-    return json.dumps(response)
+    response = {'id': user.id}
+    return jsonify(response)
 
-@app.route('/index/search', methods=['POST'])
+
+@app.route('/index/search', methods=['GET'])
 def search():
-    users = User.query.order_by(User.name).all()
+    data = request.get_json()
+
+    like_name = User.name.like(f"%{data['name']}%")
+    users = User.query.filter(like_name).order_by(User.name)
+
     return jsonify(list(map(lambda x: x.serialize(), users)))
 
-@app.route('/index/delete', methods=['POST'])
+
+@app.route('/index/delete', methods=['DELETE'])
 def delete():
     data = request.get_json()
     user = User.query.filter_by(id=data['id']).first()
-    db.session.delete(user) 
+    db.session.delete(user)
     db.session.commit()
-    return 'OK'
-@app.route('/index/update', methods=['POST'])
+
+    return jsonify({'result': f"{user.id} Removed!"})
+
+
+@app.route('/index/update', methods=['PUT'])
 def update():
     data = request.get_json()
-    print(data)
-    user = User.query.filter_by(id = data['id']).first()
-    user.name = data['name'] 
-    user.email = data['email'] 
-    user.address = data['address'] 
-    user.fone = data['phone'] 
-    db.session.add(user)
-    db.session.commit()
-    return 'OK'
 
-@app.route('/getall')
+    if data['id'] is None:
+        return jsonify({'error': 'Need id'})
+
+    user = User.query.filter_by(id=data['id']).first()
+
+    if user is not None:
+        for key in data:
+            if key in user.__dir__() and key != 'id':
+                user.__setattr__(key, data[key])
+
+        db.session.commit()
+
+        result = user.serialize()
+    else:
+        result = { 'error': 'User not found' }
+
+    return jsonify(result)
+
+
+@app.route('/getAll', methods=['GET'])
 def getAll():
-    pass
+    users = User.query.all()
+
+    return jsonify(list(map(lambda x: x.serialize(), users)))
